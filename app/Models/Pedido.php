86395286808b2c5d0e0d0ef055b8ef5e4052f43f
santiago <proyectv@gmail.com>
2026-02-19 12:23:39 +0100

    cambios

--- app/Models/Pedido.php
+++ app/Models/Pedido.php
@@ -1,14 +1,24 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pedido extends Model
 {
     protected $fillable = [
-        'email','nombre','telefono',
-        'direccion','ciudad','provincia','cp','pais',
-        'cantidad','nota','estado'
+        'email',
+        'nombre',
+        'telefono',
+        'direccion',
+        'ciudad',
+        'provincia',
+        'cp',
+        'pais',
+        'cantidad',
+        'nota',
+        'estado',
+        'observaciones'
     ];
 }
